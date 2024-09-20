35a1f6996f4b6723652bf2af70d03aebf1f46582
Janek Spaderna <janek@spaderna.eu>
2024-09-20 17:17:45 +0200

    [TUI] multiple prefix operations at once

--- exe/main.hs
+++ exe/main.hs
@@ -21,59 +21,62 @@ import Data.Bifunctor
 import Data.List.NonEmpty (nonEmpty)
 import Data.Maybe
 import Data.Text qualified as T
-import Data.These
 import Data.Void
 import Graphics.Vty qualified as V
 import Lens.Micro.Platform
 import Text.Megaparsec qualified as M
 
-data Two a = Two a a
-  deriving stock (Eq, Functor, Foldable, Traversable)
-
-_twice :: a -> Two a
-_twice a = Two a a
-
-_S :: Lens' (Two a) a
-_S = lens (\(Two a _) -> a) (\(Two _ b) a -> Two a b)
-
-_P :: Lens' (Two a) a
-_P = lens (\(Two _ b) -> b) (\(Two a _) b -> Two a b)
-
-type Selector = forall x. SimpleGetter (Two x) x
-
-class HasSelector a where
-  getSelector :: Selector
-
-instance HasSelector END where
-  getSelector = _S
-
-instance HasSelector RET where
-  getSelector = _P
-
 type Event = Void
 
+newtype SectionName = SectionName Word
+  deriving newtype (Eq, Ord, Show)
+
+nextSectionName :: SectionName -> SectionName
+nextSectionName (SectionName i) = SectionName (i + 1)
+
+data SectionLabel
+  = MainSession
+  | PrefixSection !SectionName
+  deriving stock (Eq, Ord, Show)
+
 data Name
-  = EditorS
-  | EditorP
+  = Editor !SectionLabel
+  | ErrorsVP !SectionLabel
   | KeyTable
-  | ErrorsVP
   deriving stock (Eq, Ord, Show)
 
 newtype SessionError = SessionError String
   deriving newtype (Eq)
 
 data StripResult
-  = SessionErrors (These SessionError SessionError)
+  = ParseError SessionError
   | StripError !Error
   | StripSuccess !(Session Z END)
 
+data Section = Section
+  { _secName :: !SectionName,
+    _secEditor :: Editor T.Text Name,
+    _secResult :: !(Maybe StripResult)
+  }
+
 data St = St
-  { _stEditors :: Two (Editor T.Text Name),
-    _stStripResult :: !StripResult,
+  { _stSessionEditor :: !(Editor T.Text Name),
+    _stSessionError :: !(Maybe SessionError),
+    _stSections :: [Section],
+    _stNextSectionName :: !SectionName,
     _stFocus :: !(FocusRing Name)
   }
 
 makeLenses ''St
+makeLenses ''Section
+
+findSection :: SectionName -> Traversal' St Section
+findSection name = stSections . traverse . filtered ((name ==) . view secName)
+
+sectionEditor :: SectionLabel -> Traversal' St (Editor T.Text Name)
+sectionEditor = \case
+  MainSession -> stSessionEditor
+  PrefixSection i -> findSection i . secEditor
 
 hBorderStart, hBorderEnd :: Widget n
 hBorderStart = Brick.borderElem \case
@@ -93,9 +96,6 @@ borderWithLabel lbl =
 (<++>), (<==>) :: Widget n -> Widget n -> Widget n
 a <++> b = a <+> raw (V.backgroundFill 1 1) <+> b
 a <==> b = a <=> raw (V.backgroundFill 1 1) <=> b
-
-_unused :: Widget n -> Widget n -> Widget n
-_unused = (<==>)
 
 rootAttr :: AttrName
 rootAttr = attrName "bsession"
@@ -127,16 +127,11 @@ keyTable =
 
 mainUI :: St -> Widget Name
 mainUI st = Widget Greedy Greedy do
-  render . padBottom Max . vBox $
-    [ drawEdSection _S,
-      drawEdSection _P,
-      drawResultSection (st ^. stStripResult)
-    ]
+  render . padBottom Max . foldr1 (<==>) $
+    padBottom (Pad 1) drawMainSection
+      : fmap drawPrefixSection (st ^. stSections)
   where
-    drawEdSection :: Selector -> Widget Name
-    drawEdSection sel =
-      drawEditor (Two "Session" "Prefix" ^. sel) (st ^. stEditors . sel)
-
+    drawEditor :: T.Text -> Editor T.Text Name -> Widget Name
     drawEditor name ed = withFocusRing (st ^. stFocus) (editorBorder (txt name)) ed do
       withFocusRing (st ^. stFocus) (renderEditor (txt . T.unlines)) ed
     editorBorder label hasFocus _
@@ -146,25 +141,35 @@ mainUI st = Widget Greedy Greedy do
             . borderWithLabel label
       | otherwise = borderWithLabel label
 
-    drawResultSection :: StripResult -> Widget Name
-    drawResultSection = \case
-      StripSuccess r -> borderWithLabel (txt "Result") do
+    drawMainSection :: Widget Name
+    drawMainSection =
+      vBox $
+        drawEditor "Session" (st ^. stSessionEditor)
+          : [wrapErrorsSection MainSession (str e) | SessionError e <- maybeToList (st ^. stSessionError)]
+
+    drawPrefixSection :: Section -> Widget Name
+    drawPrefixSection sec =
+      drawEditor "Prefix" (sec ^. secEditor)
+        <=> drawResultSection (PrefixSection (sec ^. secName)) (sec ^. secResult)
+
+    drawResultSection :: SectionLabel -> Maybe StripResult -> Widget Name
+    drawResultSection secLabel = \case
+      Nothing -> wrapErrorsSection secLabel (str "Errors in main session type.")
+      Just (StripSuccess r) -> borderWithLabel (txt "Result") do
         padRight Max . withAttr sessionAttr . str $ show r
-      SessionErrors es -> wrapErrorsSection $ mergeTheseWith renderError renderError (<=>) es
-        where
-          renderError (SessionError e) = padBottom (Pad 1) (str e)
-      StripError ErrorNotAPrefix -> wrapErrorsSection $ str "Invalid prefix."
-      StripError ErrorNoCont -> wrapErrorsSection $ str "No continuation."
-      StripError (ErrorContConflict s1 s2) ->
-        wrapErrorsSection . vBox $
+      Just (ParseError (SessionError e)) -> wrapErrorsSection secLabel $ padBottom (Pad 1) (str e)
+      Just (StripError ErrorNotAPrefix) -> wrapErrorsSection secLabel $ str "Invalid prefix."
+      Just (StripError ErrorNoCont) -> wrapErrorsSection secLabel $ str "No continuation."
+      Just (StripError (ErrorContConflict s1 s2)) ->
+        wrapErrorsSection secLabel . vBox $
           [ withAttr errorHeadingAttr $ str "Conflicting continuations:",
             padLeft (Pad 4) . str $ show s1,
             padLeft (Pad 4) . str $ show s2
           ]
-
-    wrapErrorsSection =
-      withVScrollBars OnRight
-        . viewport ErrorsVP Vertical
+    wrapErrorsSection secLabel =
+      vLimit 5
+        . withVScrollBars OnRight
+        . viewport (ErrorsVP secLabel) Vertical
         . padLeftRight 1
         . withAttr errorAttr
 
@@ -174,49 +179,103 @@ draw st = [mainUI st <=> cached KeyTable drawKeysTable]
     drawKeysTable =
       keyTable
         [ [("TAB", "change focus"), ("C-u", "scroll error messages 🠅")],
-          [("C-c", "quit"), ("C-d", "scroll error messages 🠇")]
+          [("C-c", "quit"), ("C-d", "scroll error messages 🠇")],
+          [("C-n", "new prefix"), ("C-x", "close current prefix")]
         ]
 
 handleEvent :: BrickEvent Name Event -> EventM Name St ()
 handleEvent = \case
   VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl]) -> halt
-  VtyEvent (V.EvKey (V.KChar 'd') [V.MCtrl]) -> vScrollPage (viewportScroll ErrorsVP) Down
-  VtyEvent (V.EvKey (V.KChar 'u') [V.MCtrl]) -> vScrollPage (viewportScroll ErrorsVP) Up
+  VtyEvent (V.EvKey (V.KChar 'd') [V.MCtrl]) -> scrollErrors Down
+  VtyEvent (V.EvKey (V.KChar 'u') [V.MCtrl]) -> scrollErrors Up
+  VtyEvent (V.EvKey (V.KChar 'n') [V.MCtrl]) -> modify \st -> reparse (PrefixSection (st ^. stNextSectionName)) $ pushEditor st
+  VtyEvent (V.EvKey (V.KChar 'x') [V.MCtrl]) -> modify dropCurrentEditor
   VtyEvent (V.EvKey (V.KChar '\t') []) -> stFocus %= focusNext
   VtyEvent (V.EvKey (V.KChar '\t') [V.MShift]) -> stFocus %= focusPrev
+  VtyEvent (V.EvKey (V.KChar '?') []) ->
+    use stFocus >>= \fr -> suspendAndResume' do
+      print fr
+      void getChar
   VtyEvent V.EvResize {} -> invalidateCache
   ev -> do
     fr <- use stFocus
     case focusGetCurrent fr of
-      Just EditorS -> zoom (stEditors . _S) $ handleEditorEvent ev
-      Just EditorP -> zoom (stEditors . _P) $ handleEditorEvent ev
+      Just (Editor secLabel) -> do
+        zoom (sectionEditor secLabel) $ handleEditorEvent ev
+        modify $ reparse secLabel
       _ -> pure ()
-    when (isJust (focusGetCurrent fr)) do
-      modify reparse
 
-reparse :: St -> St
-reparse st = st & stStripResult .~ stripResult
+scrollErrors :: Direction -> EventM Name St ()
+scrollErrors dir = do
+  focus <- use stFocus
+  case focusGetCurrent focus of
+    Just (Editor secLabel) -> vScrollPage (viewportScroll (ErrorsVP secLabel)) dir
+    _ -> pure ()
+
+pushEditor :: St -> St
+pushEditor st =
+  st
+    & stFocus .~ newFocusRing
+    & stSections %~ (++ [newSection])
+    & stNextSectionName %~ nextSectionName
   where
-    names = Two "session" "prefix"
+    newName = st ^. stNextSectionName
+    initialPrefix =
+      ".." `fromMaybe` do
+        Editor (PrefixSection i) <- focusGetCurrent (st ^. stFocus)
+        section <- st ^? findSection i
+        pure $ T.unlines $ getEditContents $ section ^. secEditor
+    newFocusRing =
+      focusRing . fmap Editor $
+        PrefixSection newName
+          : MainSession
+          : fmap (view (secName . to PrefixSection)) (st ^. stSections)
+    newSection =
+      Section
+        { _secName = st ^. stNextSectionName,
+          _secEditor = editorText (Editor (PrefixSection newName)) (Just 2) initialPrefix,
+          _secResult = Nothing -- dummy value replaced in the next `reparse` call
+        }
 
-    stripResult :: StripResult
-    stripResult =
-      either id StripSuccess . join . first SessionErrors $
-        mergeEitherWith
-          (\s p -> first StripError $ stripPrefix s p)
-          checkParse
-          checkParse
+dropCurrentEditor :: St -> St
+dropCurrentEditor st
+  | _ : _ : _ <- st ^. stSections,
+    Just focus@(Editor (PrefixSection i)) <- focusGetCurrent (st ^. stFocus) =
+      st
+        & stFocus %~ avoidMainSession . focusRing . filter (/= focus) . focusRingToList
+        & stSections %~ filter ((/= i) . view secName)
+  | otherwise = st
+  where
+    avoidMainSession focus
+      | focusGetCurrent focus == Just (Editor MainSession) = focusPrev focus
+      | otherwise = focus
 
-    checkParse :: forall a. (ParseEnd a, HasSelector a) => Either SessionError (Session Z a)
-    checkParse = case parse of
+reparse :: SectionLabel -> St -> St
+reparse section st =
+  st
+    & stSessionError .~ do Left e <- pure parsedSession; pure e
+    & case section of
+      MainSession -> stSections . each %~ \sec -> sec & secResult .~ reparseSection sec
+      PrefixSection n -> findSection n %~ \sec -> sec & secResult .~ reparseSection sec
+  where
+    reparseSection :: Section -> Maybe StripResult
+    reparseSection sec = do
+      Right session <- Just parsedSession
+      let parsed = first ParseError (checkParse $ parse "" (sec ^. secEditor))
+      let stripped = parsed >>= first StripError . stripPrefix session
+      pure $ either id StripSuccess stripped
+
+    parsedSession :: Either SessionError (Session Z END)
+    parsedSession = checkParse $ parse "session" (st ^. stSessionEditor)
+
+    checkParse :: Either ParseError (Session Z a) -> Either SessionError (Session Z a)
+    checkParse = \case
       Right s | contractive s -> Right s
-      Right _ -> Left . SessionError $ names ^. getSelector @a ++ ": session type is not contractive"
+      Right _ -> Left . SessionError $ "Session type is not contractive."
       Left pe -> Left . SessionError $ M.errorBundlePretty pe
 
-    parse :: forall a. (ParseEnd a, HasSelector a) => Either ParseError (Session Z a)
-    parse =
-      parseSession (names ^. getSelector @a) . T.intercalate "\n" . getEditContents $
-        st ^. stEditors . getSelector @a
+    parse :: forall a. (ParseEnd a) => String -> Editor T.Text Name -> Either ParseError (Session Z a)
+    parse name = parseSession name . T.intercalate "\n" . getEditContents
 
 main :: IO ()
 main = void $ defaultMain app initialState
@@ -236,18 +295,12 @@ main = void $ defaultMain app initialState
           appChooseCursor = focusRingCursor _stFocus,
           appAttrMap = const $ attrMap V.defAttr attrs
         }
-    initialState = reparse do
-      St
-        { _stEditors =
-            Two
-              (editorText EditorS (Just 2) "!Int . !Int . ?Int . end")
-              (editorText EditorP (Just 2) ".."),
-          _stStripResult = StripSuccess (SEnd END),
-          _stFocus = focusRing [EditorS, EditorP]
-        }
-
-mergeEitherWith :: (a -> b -> c) -> Either e1 a -> Either e2 b -> Either (These e1 e2) c
-mergeEitherWith _ (Left e1) (Left e2) = Left (These e1 e2)
-mergeEitherWith _ (Left e1) _ = Left (This e1)
-mergeEitherWith _ _ (Left e2) = Left (That e2)
-mergeEitherWith f (Right a) (Right b) = Right (f a b)
+    initialState =
+      reparse MainSession . pushEditor $
+        St
+          { _stSessionEditor = editorText (Editor MainSession) (Just 2) "!Int . !Int . ?Int . end",
+            _stSessionError = Nothing,
+            _stSections = [],
+            _stFocus = focusRing [],
+            _stNextSectionName = SectionName 0
+          }
